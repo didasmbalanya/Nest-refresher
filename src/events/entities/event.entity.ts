ac15bad1fe8e37a60d7a2cb3e8bb88529e29bc07
Didas Mbalanya <didasmbalanya@gmail.com>
2023-10-18 22:07:24 +0300

    add Index

--- src/events/entities/event.entity.ts
+++ src/events/entities/event.entity.ts
@@ -1,4 +1,4 @@
-import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
+import { Entity, PrimaryGeneratedColumn, Column, Index } from 'typeorm';
 
 @Entity()
 export class Event {
@@ -8,6 +8,7 @@ export class Event {
   @Column()
   type: string;
 
+  @Index()
   @Column()
   name: string;
 
