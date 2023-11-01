876fc5e930eba1fb1afaa1d7bdb8ebe0700136e9
Didas Mbalanya <didasmbalanya@gmail.com>
2023-11-01 21:54:12 +0300

    async module

--- src/app.module.ts
+++ src/app.module.ts
@@ -10,6 +10,18 @@ import appConfig from './config/app.config';
 
 @Module({
   imports: [
+    TypeOrmModule.forRootAsync({
+      useFactory: () => ({
+        type: 'postgres',
+        host: process.env.DATABASE_HOST,
+        port: +process.env.DATABASE_PORT,
+        username: process.env.DATABASE_USER,
+        password: process.env.DATABASE_PASSWORD,
+        database: process.env.DATABASE_NAME,
+        autoLoadEntities: true,
+        synchronize: true,
+      }),
+    }),
     ConfigModule.forRoot({
       load: [appConfig],
       validationSchema: Joi.object({
@@ -18,16 +30,6 @@ import appConfig from './config/app.config';
       }),
     }),
     ScentModule,
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: process.env.DATABASE_HOST,
-      port: +process.env.DATABASE_PORT,
-      username: process.env.DATABASE_USER,
-      password: process.env.DATABASE_PASSWORD,
-      database: process.env.DATABASE_NAME,
-      autoLoadEntities: true,
-      synchronize: true,
-    }),
     ScentRatingModule,
   ],
   controllers: [AppController],
