e915488741e5df55b760a12c3685f48b5acd6c87
Didas Mbalanya <didasmbalanya@gmail.com>
2023-11-08 14:05:55 +0300

    add indexing

--- src/events/entities/event.entity.ts
+++ src/events/entities/event.entity.ts
@@ -14,3 +14,13 @@ export class Event extends mongoose.Document {
 }
 
 export const EventSchema = SchemaFactory.createForClass(Event);
+EventSchema.index({ name: 1, type: -1 });
+
+/**
+ * In this example:
+ * We passed a value of 1 (to name) which specifies that the index
+ * should order these items in an Ascending order.
+ *
+ * We passed type a value of (negative) -1 which specifies that
+ * The index should order these items in Descending order.
+ */
